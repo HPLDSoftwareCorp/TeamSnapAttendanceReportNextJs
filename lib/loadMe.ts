76d75efb5723f72a749a510f4d32fb666bd2ca33
Dobes Vandermeer <dobesv@gmail.com>
2020-09-18 20:12:29 -0700

    Don't call loadMe unless we've initialized

--- lib/loadMe.ts
+++ lib/loadMe.ts
@@ -2,7 +2,8 @@ import loadTeamSnap from "./loadTeamSnap";
 
 const loadMe = async () => {
   const teamsnap = await loadTeamSnap();
+  if (!teamsnap.collections) return null;
   return teamsnap.loadMe();
-}
+};
 
 export default loadMe;
