b57682486330556815bf13713ff89cc5b11677c7
Dobes Vandermeer <dobesv@gmail.com>
2020-10-01 20:43:49 -0700

    A little cleanup

--- pages/api/checkin.ts
+++ pages/api/checkin.ts
@@ -47,55 +47,55 @@ export default async function checkin(
   req: IncomingMessage & { body: any; query: any; cookies: any },
   res: ServerResponse
 ) {
-  if (req.method === "POST") {
-    if (!ajv.validate(checkinSchema, req.body)) {
-      res.statusCode = 400;
-      res.end(ajv.errorsText());
-      return;
-    }
-    const admin = initFirebaseAdmin();
-    const firestore = admin.firestore();
-    const orgsCollection = firestore.collection("orgs");
-    const orgDoc = orgsCollection.doc(req.body.org);
-    const org = await orgDoc.get();
-    if (!org) {
-      res.statusCode = 404;
-      res.end("No such org: " + org);
-      return;
-    }
-    const checkinsCollection = orgDoc.collection("checkins");
-    await checkinsCollection.add({
-      ...req.body,
-      timestamp: new Date(req.body.timestamp),
-    });
-    const formUrl = org.get("googleFormUrl");
-    if (formUrl) {
-      const parsed = new URL(formUrl);
-      parsed.pathname = parsed.pathname.replace("/viewform", "/formResponse");
-      const qs = parsed.searchParams;
-      const subs = {
-        attendeeNames: req.body.attendeeNames,
-        contactPhoneNumber: req.body.contactPhoneNumbers.join(", "),
-        contactEmail: req.body.contactEmails.join(", "),
-        eventLocation: req.body.eventLocation,
-        eventDate: req.body.eventDate,
-        eventTime: req.body.eventTime,
-        pass: !req.body.passed ? "Fail" : "Pass",
-        userId: req.body.userId,
-      };
-      for (const [k, v] of Array.from(qs.entries())) {
-        const sub = subs[v];
-        if (sub) {
-          qs.set(k, sub);
-        }
-      }
-      qs.set("submit", "Submit");
-      const res = await axios.get(parsed.toString());
-      console.log(parsed.toString(), res.status, res.statusText);
-    }
-    res.statusCode = 200;
-  } else {
+  if (req.method !== "POST") {
     res.statusCode = 405;
+    res.end();
+    return;
   }
+  if (!ajv.validate(checkinSchema, req.body)) {
+    res.statusCode = 400;
+    res.end(ajv.errorsText());
+    return;
+  }
+  const admin = initFirebaseAdmin();
+  const firestore = admin.firestore();
+  const orgsCollection = firestore.collection("orgs");
+  const orgDoc = orgsCollection.doc(req.body.org);
+  const org = await orgDoc.get();
+  if (!org) {
+    res.statusCode = 404;
+    res.end("No such org: " + org);
+    return;
+  }
+  const checkinsCollection = orgDoc.collection("checkins");
+  await checkinsCollection.add({
+    ...req.body,
+    timestamp: new Date(req.body.timestamp),
+  });
+  const formUrl = org.get("googleFormUrl");
+  if (formUrl) {
+    const parsed = new URL(formUrl);
+    parsed.pathname = parsed.pathname.replace("/viewform", "/formResponse");
+    const qs = parsed.searchParams;
+    const subs = {
+      attendeeNames: req.body.attendeeNames,
+      contactPhoneNumber: req.body.contactPhoneNumbers.join(", "),
+      contactEmail: req.body.contactEmails.join(", "),
+      eventLocation: req.body.eventLocation,
+      eventDate: req.body.eventDate,
+      eventTime: req.body.eventTime,
+      pass: !req.body.passed ? "Fail" : "Pass",
+      userId: req.body.userId,
+    };
+    for (const [k, v] of Array.from(qs.entries())) {
+      const sub = subs[v];
+      if (sub) {
+        qs.set(k, sub);
+      }
+    }
+    qs.set("submit", "Submit");
+    const res = await axios.get(parsed.toString());
+  }
+  res.statusCode = 200;
   res.end();
 }
