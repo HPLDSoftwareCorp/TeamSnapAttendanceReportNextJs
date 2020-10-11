4be3b0f0948e5b8c82ecf9ee936f8e703be00dcd
Dobes Vandermeer <dobesv@gmail.com>
2020-10-10 23:52:22 -0700

    Try to fix oauth callback in popup mode

--- pages/oauth-callback.tsx
+++ pages/oauth-callback.tsx
@@ -6,17 +6,21 @@ import exchangeCodeForToken, {
 } from "../lib/server/teamsnap/exchangeCodeForToken";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const data = await exchangeCodeForToken(
-    context.query.code as string,
-    `https://${context.req.headers.host}/oauth-callback`
-  );
-  return { props: data };
+  console.log(context.req.url, context.query, context.params);
+  if (context.query.code) {
+    const data = await exchangeCodeForToken(
+      context.query.code as string,
+      `https://${context.req.headers.host}/oauth-callback`
+    );
+    return { props: data };
+  }
+  return { props: {} };
 };
 
 export default function OAuthCallback({ access_token }: OAuthTokenData) {
   const router = useRouter();
   const stateArgs = new URLSearchParams(router.query.state as string);
-  const returnUrl = stateArgs.get('returnUrl') || router.query.returnUrl || "/";
+  const returnUrl = stateArgs.get("returnUrl") || router.query.returnUrl || "/";
   if (process.browser && access_token) {
     sessionStorage["teamsnap.authToken"] = access_token;
     window.location.href = returnUrl as string;
